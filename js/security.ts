36772177f92defaee83b50ad3c762ea6b0c388da
sabflik <szaf045@aucklanduni.ac.nz>
2016-09-02 00:19:04 +1200

    Javascript was acting up. Fixed the camera highlighting
    For loop stopped working

--- js/security.ts
+++ js/security.ts
@@ -1,11 +1,37 @@
 var roomList = $("#room-list")[0];
 var rooms: NodeListOf<HTMLElement> = roomList.getElementsByTagName('li');
 
-for (var i: number = 1; i < rooms.length; i++) {
-    rooms[i].addEventListener("click", function () {
-        $(document).ready(function () {
-            $('#room-list ul li').removeClass("active");
-            rooms[i].setAttribute("class", "active");
-        });
+rooms[1].addEventListener("click", function () {
+    $(document).ready(function () {
+        $('#room-list ul li').removeClass("active");
+        rooms[1].setAttribute("class", "active");
     });
-}
+});
+
+rooms[2].addEventListener("click", function () {
+    $(document).ready(function () {
+        $('#room-list ul li').removeClass("active");
+        rooms[2].setAttribute("class", "active");
+    });
+});
+
+rooms[3].addEventListener("click", function () {
+    $(document).ready(function () {
+        $('#room-list ul li').removeClass("active");
+        rooms[3].setAttribute("class", "active");
+    });
+});
+
+rooms[4].addEventListener("click", function () {
+    $(document).ready(function () {
+        $('#room-list ul li').removeClass("active");
+        rooms[4].setAttribute("class", "active");
+    });
+});
+
+rooms[5].addEventListener("click", function () {
+    $(document).ready(function () {
+        $('#room-list ul li').removeClass("active");
+        rooms[5].setAttribute("class", "active");
+    });
+});
