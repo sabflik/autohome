70c7fe90cb96def3390f65490083ce176ddc60b7
sabflik <szaf045@aucklanduni.ac.nz>
2016-09-02 00:26:12 +1200

    Changed from typescript to javascript onclick

--- js/security.ts
+++ js/security.ts
@@ -1,37 +1,47 @@
-var roomList = $("#room-list")[0];
-var rooms: NodeListOf<HTMLElement> = roomList.getElementsByTagName('li');
+function activeFunction() {
+	$(document).ready(function(){
+	$('#room-list ul li').click(function(){
+		$('#room-list ul li').removeClass("active");
+		$(this).addClass("active");
+		});
+	});
+}
 
-rooms[1].addEventListener("click", function () {
-    $(document).ready(function () {
-        $('#room-list ul li').removeClass("active");
-        rooms[1].setAttribute("class", "active");
-    });
-});
 
-rooms[2].addEventListener("click", function () {
-    $(document).ready(function () {
-        $('#room-list ul li').removeClass("active");
-        rooms[2].setAttribute("class", "active");
-    });
-});
+// var roomList = $("#room-list")[0];
+// var rooms: NodeListOf<HTMLElement> = roomList.getElementsByTagName('li');
 
-rooms[3].addEventListener("click", function () {
-    $(document).ready(function () {
-        $('#room-list ul li').removeClass("active");
-        rooms[3].setAttribute("class", "active");
-    });
-});
+// rooms[1].addEventListener("click", function () {
+//     $(document).ready(function () {
+//         $('#room-list ul li').removeClass("active");
+//         rooms[1].setAttribute("class", "active");
+//     });
+// });
 
-rooms[4].addEventListener("click", function () {
-    $(document).ready(function () {
-        $('#room-list ul li').removeClass("active");
-        rooms[4].setAttribute("class", "active");
-    });
-});
+// rooms[2].addEventListener("click", function () {
+//     $(document).ready(function () {
+//         $('#room-list ul li').removeClass("active");
+//         rooms[2].setAttribute("class", "active");
+//     });
+// });
 
-rooms[5].addEventListener("click", function () {
-    $(document).ready(function () {
-        $('#room-list ul li').removeClass("active");
-        rooms[5].setAttribute("class", "active");
-    });
-});
+// rooms[3].addEventListener("click", function () {
+//     $(document).ready(function () {
+//         $('#room-list ul li').removeClass("active");
+//         rooms[3].setAttribute("class", "active");
+//     });
+// });
+
+// rooms[4].addEventListener("click", function () {
+//     $(document).ready(function () {
+//         $('#room-list ul li').removeClass("active");
+//         rooms[4].setAttribute("class", "active");
+//     });
+// });
+
+// rooms[5].addEventListener("click", function () {
+//     $(document).ready(function () {
+//         $('#room-list ul li').removeClass("active");
+//         rooms[5].setAttribute("class", "active");
+//     });
+// });
