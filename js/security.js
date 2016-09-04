661b8699e12eb376cdeb54740b58cc4445cba56b
sabflik <szaf045@aucklanduni.ac.nz>
2016-09-05 11:29:49 +1200

    Moved javascript logic in "security"
    Moved from head tags in html doc to its own javascript doc

--- js/security.js
+++ js/security.js
@@ -6,6 +6,26 @@
 // 		});
 // 	});
 // }
+function activeFunction(room) {
+    $(document).ready(function () {
+        $('#room-list ul li').removeClass("active");
+        $(document.getElementById(room)).addClass("active");
+    });
+}
+$(function () {
+    $('#alarm').change(function () {
+        if ($(this).prop("checked") == true) {
+            $('#danger').fadeIn("slow");
+            $('#danger').html('You have DISARMED the house!');
+            $("#danger").fadeOut(2500, 'swing');
+        }
+        else {
+            $('#success').fadeIn("slow");
+            $('#success').html('You have ARMED the house!');
+            $("#success").fadeOut(2500, 'swing');
+        }
+    });
+});
 // var roomList = $("#room-list")[0];
 // var rooms: NodeListOf<HTMLElement> = roomList.getElementsByTagName('li');
 // rooms[1].addEventListener("click", function () {
