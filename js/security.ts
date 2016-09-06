4d0bcee619d4a76606b574ba333c229283b0696a
sabflik <szaf045@aucklanduni.ac.nz>
2016-09-06 22:28:19 +1200

    Removed commented out parts

--- js/security.ts
+++ js/security.ts
@@ -1,12 +1,3 @@
-// function activeFunction() {
-// 	$(document).ready(function(){
-// 	$('#room-list ul li').click(function(){
-// 		$('#room-list ul li').removeClass("active");
-// 		$(this).addClass("active");
-// 		});
-// 	});
-// }
-
 function activeFunction(room) {
     $(document).ready(function () {
         $('#room-list ul li').removeClass("active");
@@ -27,41 +18,3 @@ $(function () {
         }
     })
 })
-
-// var roomList = $("#room-list")[0];
-// var rooms: NodeListOf<HTMLElement> = roomList.getElementsByTagName('li');
-
-// rooms[1].addEventListener("click", function () {
-//     $(document).ready(function () {
-//         $('#room-list ul li').removeClass("active");
-//         rooms[1].setAttribute("class", "active");
-//     });
-// });
-
-// rooms[2].addEventListener("click", function () {
-//     $(document).ready(function () {
-//         $('#room-list ul li').removeClass("active");
-//         rooms[2].setAttribute("class", "active");
-//     });
-// });
-
-// rooms[3].addEventListener("click", function () {
-//     $(document).ready(function () {
-//         $('#room-list ul li').removeClass("active");
-//         rooms[3].setAttribute("class", "active");
-//     });
-// });
-
-// rooms[4].addEventListener("click", function () {
-//     $(document).ready(function () {
-//         $('#room-list ul li').removeClass("active");
-//         rooms[4].setAttribute("class", "active");
-//     });
-// });
-
-// rooms[5].addEventListener("click", function () {
-//     $(document).ready(function () {
-//         $('#room-list ul li').removeClass("active");
-//         rooms[5].setAttribute("class", "active");
-//     });
-// });
