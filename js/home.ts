3d54716a88cb6bf635dfbaec07b2d01a40f3bdf4
sabflik <szaf045@aucklanduni.ac.nz>
2016-09-09 17:07:18 +1200

    Website uses Bing Web Search API

--- js/home.ts
+++ js/home.ts
@@ -5,3 +5,48 @@ $(function() : void {
 		isVertical: true
 	});
 });
+
+let input = document.getElementById("#search-input");
+
+function searchFunction() : void{
+	var query : string = $(input).val();
+
+var params = {
+            // Request parameters
+            "q": "bill gates",
+            "count": "5",
+            "offset": "0",
+            "mkt": "en-us",
+            "safesearch": "Moderate",
+        };
+      
+        $.ajax({
+            url: "https://api.cognitive.microsoft.com/bing/v5.0/search?" + $.param(params),
+            beforeSend: function(xhrObj){
+                // Request headers
+                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","90ed9ee35654423087ed19c0acd3d305");
+            },
+            type: "GET",
+            // Request body
+            data: "{body}",
+        })
+        .done(function(data) {
+			var obj = data;
+			var displayText : string = "Related Websites: \n\n"
+			+obj.webPages.value[0].name+"\n"
+			+obj.webPages.value[0].displayUrl+"\n\n"
+			+obj.webPages.value[1].name+"\n"
+			+obj.webPages.value[1].displayUrl+"\n\n"
+			+obj.webPages.value[2].name+"\n"
+			+obj.webPages.value[2].displayUrl+"\n\n"
+			+obj.webPages.value[3].name+"\n"
+			+obj.webPages.value[3].displayUrl+"\n\n"
+			+obj.webPages.value[4].name+"\n"
+			+obj.webPages.value[4].displayUrl;
+			alert(displayText);
+        })
+        .fail(function() {
+            alert("error");
+        });
+
+}
