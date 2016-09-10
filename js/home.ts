bda6ca13e454f8065e5077c2e171362623228b8d
sabflik <szaf045@aucklanduni.ac.nz>
2016-09-10 17:51:05 +1200

    Added dynamic calls to bing web search

--- js/home.ts
+++ js/home.ts
@@ -1,51 +1,49 @@
-$(function() : void {
-	$('.kwicks').kwicks({
-		maxSize : 200,
-		behavior: 'menu',
-		isVertical: true
-	});
+$(function (): void {
+    $('.kwicks').kwicks({
+        maxSize: 200,
+        behavior: 'menu',
+        isVertical: true
+    });
 });
 
-let input = document.getElementById("#search-input");
+function searchFunction(): void {
+    var query: string = $("#search-input").val();
 
-function searchFunction() : void{
-	var query : string = $(input).val();
+    var params = {
+        // Request parameters
+        "q": query,
+        "count": "5",
+        "offset": "0",
+        "mkt": "en-us",
+        "safesearch": "Moderate",
+    };
 
-var params = {
-            // Request parameters
-            "q": "bill gates",
-            "count": "5",
-            "offset": "0",
-            "mkt": "en-us",
-            "safesearch": "Moderate",
-        };
-      
-        $.ajax({
-            url: "https://api.cognitive.microsoft.com/bing/v5.0/search?" + $.param(params),
-            beforeSend: function(xhrObj){
-                // Request headers
-                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","90ed9ee35654423087ed19c0acd3d305");
-            },
-            type: "GET",
-            // Request body
-            data: "{body}",
+    $.ajax({
+        url: "https://api.cognitive.microsoft.com/bing/v5.0/search?" + $.param(params),
+        beforeSend: function (xhrObj) {
+            // Request headers
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "90ed9ee35654423087ed19c0acd3d305");
+        },
+        type: "GET",
+        // Request body
+        data: "{body}",
+    })
+        .done(function (data) {
+            var obj = data;
+            var displayText: string = "Related Websites: \n\n"
+                + obj.webPages.value[0].name + "\n"
+                + obj.webPages.value[0].displayUrl + "\n\n"
+                + obj.webPages.value[1].name + "\n"
+                + obj.webPages.value[1].displayUrl + "\n\n"
+                + obj.webPages.value[2].name + "\n"
+                + obj.webPages.value[2].displayUrl + "\n\n"
+                + obj.webPages.value[3].name + "\n"
+                + obj.webPages.value[3].displayUrl + "\n\n"
+                + obj.webPages.value[4].name + "\n"
+                + obj.webPages.value[4].displayUrl;
+            alert(displayText);
         })
-        .done(function(data) {
-			var obj = data;
-			var displayText : string = "Related Websites: \n\n"
-			+obj.webPages.value[0].name+"\n"
-			+obj.webPages.value[0].displayUrl+"\n\n"
-			+obj.webPages.value[1].name+"\n"
-			+obj.webPages.value[1].displayUrl+"\n\n"
-			+obj.webPages.value[2].name+"\n"
-			+obj.webPages.value[2].displayUrl+"\n\n"
-			+obj.webPages.value[3].name+"\n"
-			+obj.webPages.value[3].displayUrl+"\n\n"
-			+obj.webPages.value[4].name+"\n"
-			+obj.webPages.value[4].displayUrl;
-			alert(displayText);
-        })
-        .fail(function() {
+        .fail(function () {
             alert("error");
         });
 
